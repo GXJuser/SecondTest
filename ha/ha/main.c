aab8ad4875876351939a58097c8c9f065fbe9d78
GXJuser <945533678@qq.com>
2016-01-08 16:21:03 +0800

    第一次

---
+++ ha/ha/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ha
+//
+//  Created by lk on 16/1/8.
+//  Copyright © 2016年 lk. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, ha!\n");
+    return 0;
+}
